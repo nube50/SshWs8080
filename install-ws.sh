10cda6653e176388a1b68419029e05ef5e09e1e5
ETH973 <39785017+ETH973@users.noreply.github.com>
2025-03-23 03:54:37 -0500

    Add files via upload

---
+++ install-ws.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Función para imprimir mensajes
+print_message() {
+    echo ""
+    echo "$1"
+    echo ""
+}
+# Actualizar e instalar paquetes
+print_message "Descargando paquetes necesarios"
+sudo apt update
+sudo apt install -y screen python2
+# Descargar archivo Python
+print_message "Descargando archivo python"
+sudo mkdir -p /etc/SSH-ws
+wget raw.githubusercontent.com/ETH973/Sshws80/main/websocket973.py -O /etc/SSH-ws/websocket973.py
+# Dar permisos de ejecución al archivo
+sudo chmod +x /etc/SSH-ws/websocket973.py
+# Limpiar la pantalla
+clear
+# Iniciar el servicio en una sesión de screen
+print_message "Iniciando servicio en screen"
+screen -dmS ws_tunnel python2 /etc/SSH-ws/websocket973.py 80
+# Mensaje final
+print_message "Instalación completada :)"
