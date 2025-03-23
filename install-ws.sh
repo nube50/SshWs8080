b6227cbb5aff2bcfb5207f087f1d77ba4303266e
nube50 <dajomondra@gmail.com>
2025-03-23 14:26:09 -0500

    Update install-ws.sh

--- install-ws.sh
+++ install-ws.sh
@@ -2,5 +2,5 @@
 apt update -y
 apt install -y python2
 chmod +x websocket973.py
-python2 websocket973.py 8080
 useradd -e 2025-12-31 Hola && echo "Hola:Mundo" | chpasswd
+python2 websocket973.py 8080
